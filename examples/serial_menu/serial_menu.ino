8841fd5630ef0324c6ddada608a036b0478b73a0
Rui Azevedo <ruihfazevedo@gmail.com>
2016-10-24 21:22:44 +0000

    enahnced serial demo (testing)

--- examples/serial_menu/serial_menu.ino
+++ examples/serial_menu/serial_menu.ino
@@ -16,22 +16,81 @@ Extensible: Yes
 #include <menuPrint.h>
 //#include <streamFlow.h>
 
-int param;
+#define LEDPIN 13
+
+//aux vars
+int ledCtrl=0;
+int percent;//just testing changing this var
+double fps=0;
+int counter=0;
+
+///////////////////////////////////////////////////////////////////////////
+//functions to wire as menu actions
+bool ledOn() {
+  Serial.println("set led on!");
+  digitalWrite(LEDPIN,ledCtrl=1);
+  return false;
+}
+
+bool ledOff() {
+  Serial.println("set led off!");
+  digitalWrite(LEDPIN,ledCtrl=0);
+  return false;
+}
+
+bool quit() {
+  Serial.println("Quiting after action call");
+  return true;
+}
 
 /////////////////////////////////////////////////////////////////////////
 // MENU DEFINITION
 // here we define the menu structure and wire actions functions to it
-bool test=false;
-TOGGLE(test,onoff_tog,"led: ",
-  VALUE("On",HIGH),
-  VALUE("Off",LOW)
+// empty options are just for scroll testing
+
+/*bool setLed() {
+  digitalWrite(LEDPIN,ledCtrl);
+  return false;
+}*/
+TOGGLE(ledCtrl,setLed,"Led: ",
+    VALUE("On",HIGH,ledOn),
+    VALUE("Off",LOW,ledOff)
 );
 
+int selTest=0;
+SELECT(selTest,selMenu,"Select",
+  VALUE("Zero",0),
+  VALUE("One",1),
+  VALUE("Two",2)
+);
+
+int chooseTest=-1;
+CHOOSE(chooseTest,chooseMenu,"Choose ",
+  VALUE("First",1),
+  VALUE("Second",2),
+  VALUE("Third",3),
+  VALUE("Last",-1)
+);
+
+MENU(subMenu,"SubMenu"
+  ,OP("A",quit)
+  ,OP("B",quit)
+  ,OP("C",quit)
+  ,OP("D",quit)
+  ,OP("E",quit)
+  ,OP("F",quit)
+  ,OP("G",quit)
+  ,OP("H",quit)
+);
 
 MENU(mainMenu,"Main menu",
-  OP("Disabled option",menu::nothing),
-  SUBMENU(onoff_tog),
-  FIELD(param,"Name","%",0,100,10,1)
+  SUBMENU(setLed),
+  OP("LED On",ledOn),
+  OP("LED Off",ledOff),
+  SUBMENU(selMenu),
+  SUBMENU(chooseMenu),
+  SUBMENU(subMenu),
+  FIELD(percent,"Percent","%",0,100,10,1)
 );
 
 menuPrint menu_out(Serial);//describe output device
@@ -40,11 +99,11 @@ void setup() {
   Serial.begin(115200);
   while(!Serial);
   Serial.println("menu system test");
-  pinMode(13,OUTPUT);
+  pinMode(LEDPIN,OUTPUT);
   mainMenu.data[0]->enabled=false;
 }
 
 void loop() {
   mainMenu.poll(menu_out,Serial);
-  digitalWrite(13,test);
+  digitalWrite(LEDPIN,ledCtrl);
 }
