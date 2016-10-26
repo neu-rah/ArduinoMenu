ccd89db92ef3960ac62629b4b8a310167f199304
Rui Azevedo <ruihfazevedo@gmail.com>
2016-10-26 16:50:48 +0000

    added Blink example removed PC dead code

---
+++ examples/Blink/Blink.ino
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <menu.h>
+
+#define LEDPIN 13
+
+int timeOn=500;
+int timeOff=500;
+
+MENU(mainMenu, "Blink menu", doNothing, noEvent, wrapStyle
+  ,FIELD(timeOn,"On","ms",0,1000,100,10,doNothing,noEvent)
+  ,FIELD(timeOff,"Off","ms",0,1000,100,10,doNothing,noEvent)
+  ,EXIT("<Back")
+);
+
+MENU_INPUTS(in,&Serial);
+
+serialOut outSerial(Serial);//,serial_panels);//the output device (just the serial port)
+
+MENU_OUTPUTS(out,&outSerial);
+NAVROOT(nav,mainMenu,1,in,out);
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);
+  pinMode(LEDPIN, OUTPUT);
+}
+
+void loop() {
+  nav.poll();
+  digitalWrite(LEDPIN, HIGH);
+  delay(timeOn);
+  digitalWrite(LEDPIN, LOW);
+  delay(timeOff);
+}
