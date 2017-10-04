93ef337597a8ac1f7d5d6f82569eab033a6345e0
Rui Azevedo <ruihfazevedo@gmail.com>
2017-10-04 07:36:00 +0000

    fix teensy serial class for serialIn

--- src/menuIO/serialIn.h
+++ src/menuIO/serialIn.h
@@ -10,11 +10,17 @@ HardwareSerial input driver
   #define RSITE_ARDUINO_MENU_HW_SERIALIN
   #include "../menu.h"
 
+  #if defined(CORE_TEENSY)
+    #define SERIAL_CLASS usb_serial_class
+  #else
+    #define SERIAL_CLASS HardwareSerial
+  #endif
+
   namespace Menu {
     class serialIn:public menuIn {
       public:
-        HardwareSerial& in;
-        serialIn(HardwareSerial& serial):in(serial) {}
+        SERIAL_CLASS& in;
+        serialIn(SERIAL_CLASS& serial):in(serial) {}
         size_t write(uint8_t o) override {return in.write(o);}
         int available() override {return in.available();}
         int peek() override {return in.peek();}
