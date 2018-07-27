7a8f3a4b8485cea40c717d9aacae9cc3157a67a0
Rui Azevedo <ruihfazevedo@gmail.com>
2018-07-27 02:17:23 +0000

    cosmetics

--- src/menuDefs.h
+++ src/menuDefs.h
@@ -1,15 +1,4 @@
 /* -*- C++ -*- */
-/********************
-Sept 2014 ~ Oct. 2017 Rui Azevedo - ruihfazevedo(@rrob@)gmail.com
-
-menu library for the arduino framework
-
-www.r-site.net
-
-creative commons license 4.0: CC BY-NC-SA
-This software is furnished "as is", without technical support, and with no
-warranty, express or implied, as to its usefulness for any purpose.
-***/
 
 #pragma once
 
