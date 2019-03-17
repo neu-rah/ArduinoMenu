2041d1eadf7ff916d8f3a438292ca91a608d1d40
Rui Azevedo <ruihfazevedo@gmail.com>
2019-03-17 01:38:05 -0100

    runs on nano (tiny.ino)

--- src/defs/tiny.h
+++ src/defs/tiny.h
@@ -3,30 +3,29 @@
 #include <menu.h>
 
 /*
-simplest text menu, ram based
+simplest text menu
+ram based
 static size and options structure
 no associated actions
 no accels
+no menu titles
 raw print to serial
 */
 using Sys=SysDef<decltype(Serial),uint8_t>;
 using idx_t=Sys::Idx;//its uint8_t we just sent it
 
-template<typename O> using Prompt=Sys::Prompt<O>;
-using Static=Sys::Static<>;
-
-template<const char** title>
-using Title=Sys::asTitle<Sys::Static<>::Text<title[0]>>;
+// template<typename O> using Prompt=Sys::Prompt<O>;
+// using Static=Sys::Static<>;
 
 template<const char** text>
-using OP=Prompt<
-  Sys::asTitle<
-    Sys::Static<>::Text<text[0]>
-  >
+using Title=Sys::asTitle<Sys::Static<>::Text<text[0]>>;
+
+template<const char** text>
+using OP=Sys::Prompt<
+  Title<text[0]>
 >;
 
 template<const char** title,idx_t n>
-using MENU=Prompt<
-  Sys::Static<
-    Sys::asTitle<Sys::Static<>::Text<title[0]>>
-  >::Menu<n>;
+using MENU=Sys::Prompt<
+  Sys::Static<>::Menu<n>
+>;
