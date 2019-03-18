891a46426cf02edde26034d4623a87b349bc6209
Rui Azevedo <ruihfazevedo@gmail.com>
2019-03-18 03:31:24 -0100

    great!

---
+++ src/component/VectorMenu.h
@@ -0,0 +1,26 @@
+/* -*- C++ -*- */
+#pragma once
+#include <vector>
+using namespace std;
+#include <menu.h>
+
+template<typename Out,typename O>
+class VectorMenuDef:public O {
+//straight transpose objectpreserving templates... on production code we will be more succinct
+public:
+  using Def=MenuSystemDef<Out>;
+  using Base=typename Def::Base;
+  template<typename T>
+  using Item=typename Def::template Item<T>;
+
+  VectorMenuDef(initializer_list<Base*> oo):data(oo) {}
+  VectorMenuDef(const char*title,initializer_list<Base*> oo):O(title),data(oo) {}
+  template<typename... OO>
+  VectorMenuDef(const char*title,OO... oo):O(title),data{oo...} {}
+  template<typename... OO>
+  VectorMenuDef(OO... oo):data{oo...} {}
+  inline size_t size() const {return data.size();}
+  inline Base& operator[](size_t n) const {return *data[n];}
+protected:
+  vector<Base*> data;
+};
---
+++ src/menu.h
@@ -0,0 +1,91 @@
+/* -*- C++ -*- */
+#pragma once
+
+#if defined(MENU_DEBUG) && defined(TRACE)
+  #define trace(x) x
+#else
+  #define trace(x)
+#endif
+#ifdef MENU_DEBUG
+  #define _trace(x) x
+#else
+  #define _trace(x)
+#endif
+#ifndef MENU_RELEASE
+  #define __trace(x) x
+#else
+  #define __trace(x)
+#endif
+
+template<typename Out>
+struct MenuSystemDef {
+  //////////////////////////////////////////////////
+  // interface
+  struct Base {
+    inline virtual Out& operator<<(Out& o) const {return o;}
+    inline virtual size_t size() const {return 0;}
+    inline virtual Base& operator[](size_t n) const {
+      _trace(MENU_DEBUG_OUT<<"Item not available as non-constant"<<endl);
+      throw 1;
+    }
+  };
+
+  //adapter
+  template<typename O>
+  struct Item:public Base,public O {
+    using O::O;
+
+    template<typename... OO>
+    inline Item(OO... oo):O(oo...) {}
+    template<typename... OO>
+    inline Item(const char*title,OO... oo):O(title,oo...) {}
+    inline Item(const char*title):O(title) {}
+
+    inline Out& operator<<(Out& o) const override {return O::out(o);}
+    inline size_t size() const override {return O::size();}
+    inline Base& operator[](size_t n) const override {return O::operator[](n);}
+  };
+
+  /////////////////////////////////////////////////////////
+  //static routers
+  struct Empty {
+    static inline Out& out(Out& o) {return o;}
+    static inline size_t size() {return 0;}
+    inline Base& operator[](size_t n) const {
+      _trace(MENU_DEBUG_OUT<<"Item not available as non-constant"<<endl);
+      throw 1;
+    }
+  };
+
+  template<const char** text,typename O=Empty>
+  struct StaticText:public O {
+    static inline Out& out(Out& o) {return o<<text[0];}
+  };
+
+  template<typename O=Empty>
+  class Text:public O {
+  protected:
+    const char *text;
+  public:
+    Text(const char* t):text(t) {}
+    inline Out& out(Out& o) const {return o<<text;}
+  };
+
+  template<size_t n,typename O=Empty>
+  class StaticMenu:public O {
+  protected:
+    Base* data[n];
+  public:
+    template<typename... OO>
+    inline StaticMenu(OO... oo):data{oo...} {}
+    template<typename... OO>
+    inline StaticMenu(const char*title,OO... oo):O(title),data{oo...} {}
+    static inline size_t size() {return n;}
+    inline Base& operator[](size_t i) const {return *data[i];}
+  };
+};
+
+template<typename Out,typename O>
+inline Out& operator<<(Out& out,O& item) {
+  return item.operator<<(out);
+}
