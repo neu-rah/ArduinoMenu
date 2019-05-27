207b124ae266820a016739c66b8fbf7f7d18d9bb
Rui Azevedo <ruihfazevedo@gmail.com>
2019-05-27 04:20:58 +0000

    reduced navugation objects

--- src/menu/nav.h
+++ src/menu/nav.h
@@ -19,6 +19,7 @@ template<typename N=Nil> struct Drift:public N {
   constexpr static inline bool selected(idx_t) {return false;}
   constexpr static inline bool enabled(idx_t) {return true;}
   constexpr static inline Modes mode() {return Modes::Normal;}
+  constexpr static inline bool hasFocus() {return false;}
   template<typename Nav> constexpr static inline Modes _mode(Nav&) {return Modes::Normal;}
   template<typename Nav> constexpr static inline Modes mode(Nav& nav) {return nav._mode();}
   template<typename Nav> constexpr static inline bool _up(Nav& nav) {return false;}
@@ -27,67 +28,51 @@ template<typename N=Nil> struct Drift:public N {
   template<typename Nav> constexpr static inline bool _right(Nav& nav) {return false;}
   template<typename Nav> constexpr static inline bool _enter(Nav& nav) {return false;}
   template<typename Nav> constexpr static inline bool _esc(Nav& nav) {return false;}
-  // template<typename Nav> constexpr static inline bool up(Nav& nav) {return nav._up(nav);}
-  // template<typename Nav> constexpr static inline bool down(Nav& nav) {return nav._down(nav);}
-  // template<typename Nav> constexpr static inline bool left(Nav& nav) {return nav._left(nav);}
-  // template<typename Nav> constexpr static inline bool right(Nav& nav) {return nav._right(nav);}
-  // template<typename Nav> constexpr static inline bool enter(Nav& nav) {return nav._enter(nav);}
-  // template<typename Nav> constexpr static inline bool esc(Nav& nav) {return nav._esc(nav);}
 };
 
 /**
 * The NavBase class. Provides common navigation functionality
 * @brief this is a brief
 */
-template<typename Out,typename N=Drift<>>
+template<typename Out,typename Data,typename N=Drift<>>
 class NavBase:public N {
   public:
+    using This=NavBase<Out,Data,N>;
+    using OutType=Out;
+    using DataType=Data;
+    NavBase():data(NULL) {}
+    NavBase(Data& o):data(&o) {}
     inline void enterMenuRender() {onMenu=true;}
     inline void exitMenuRender() {onMenu=false;}
     inline idx_t top() const {return out.top();}
     inline void setTop(idx_t n) {out.setTop(n);}
     inline idx_t height() const {return out.height();}
-    // inline void setFocus(Item*i=NULL) {focus=i;}
-    // inline void clearFocus() {setFocus();}
-    // inline Item& getFocus() const {return *focus;}
-    // inline bool hasFocus() {return focus!=NULL;}
+    inline void setTarget(Data& d) {data=&d;}
+    template<typename Nav>
+    inline void printMenu(Nav& nav) {
+      out.template printMenu<Nav,Out,Data>(nav,out,*data);
+    }
+    inline idx_t size() {return data->size();}
+    inline void enable(idx_t n,bool o) {data->enable(n,o);}
+    inline bool enabled(idx_t n) {return data->enabled(n);}
+    inline NavAgent activate() {return data->activate();}
+    inline NavAgent activate(idx_t n) {return data->activateItem(n);}
+    template<typename Nav>
+    inline Modes _mode(Nav&) const {return data->mode();}
+    inline void newView() {out.newView();}
   protected:
     Out out;
     bool onMenu=false;
-};
-
-/*
-* The StaticNav class. Is bound to a specific menu data
-* this class does not offer much saving in relation to DynamicNav
-* except for not using virtuals
-*/
-template<typename Out,typename Data,typename N=Drift<>>
-class StaticNav:public NavBase<Out,N> {
-  public:
-    using Base=NavBase<Out,N>;
-    using This=StaticNav<Out,Data,N>;
-    inline void setTarget(Data d) {data=d;}
-    template<typename Nav>
-    inline void printMenu(Nav& nav) {
-      Base::out.template printMenu<Nav,Out,Data>(nav,Base::out,data);
-    }
-    inline idx_t size() {return data.size();}
-    inline void enable(idx_t n,bool o) {data.enable(n,o);}
-    inline bool enabled(idx_t n) {return data.enabled(n);}
-    // inline Item& operator[](idx_t n) {return data.operator[](n);}
-    inline NavAgent activate() {return data.activate();}
-    inline NavAgent activate(idx_t n) {return data.activateItem(n);}
-    inline Modes _mode() const {return data.mode();}
-  protected:
-    Data data;
+    Data* data;
 };
 
 /*
 * The NavRoot class encapsulates navigation components and implements/redirects navigation interface
 * use dynamic nav instead
 */
-/*template<typename N>
+template<typename N>
 struct NavRoot:public N {
+  using N::N;
   using This=NavRoot<N>;
   inline bool up() {return N::template _up<This>(*this);}
   inline bool down() {return N::template _down<This>(*this);}
@@ -97,45 +82,35 @@ struct NavRoot:public N {
     trace(MDO<<"NavRoot::enter"<<endl);
     return N::template _enter<This>(*this);}
   inline bool esc() {return N::template _esc<This>(*this);}
-  inline Modes mode() {
-    _trace(MDO<<"NavRoot::mode"<<endl);
-    return N::template _mode<This>(*this);}
+  inline Modes mode() {return N::_mode(*this);}
+    // _trace(MDO<<"NavRoot::mode"<<endl);
+    // return N::template _mode<This>(*this);}
   // inline NavAgent activate() {return N::activate();}
   inline void printMenu() {
     N::enterMenuRender();
-    N::out.newView();
+    N::newView();
     N::template printMenu<This>(*this);
     N::exitMenuRender();
   }
-};*/
+};
 
 /**
 * The DynamicNav class. Can point o other target menu
 */
 template<typename Out,typename Data,typename N=Drift<>>
-class Nav:public NavNode,public NavBase<Out,N> {
+class DynamicNav:public NavNode,public NavRoot<NavBase<Out,Data,N>> {
   public:
-    using Base=NavBase<Out,N>;
+    using Base=NavRoot<NavBase<Out,Data,N>>;
     using This=DynamicNav<Out,Data,N>;
-    DynamicNav(Data& o):data(&o) {}
-    inline void setTarget(Data d) {data=d;}
-    inline void printMenu() {This::printMenu(*this);}
-    template<typename Nav>
-    inline void printMenu(Nav& nav) {
-      // Base::enterMenuRender();
-      // Base::out.newView();
-      Base::out.template printMenu<Nav,Out,Data>(nav,Base::out,*data);
-      // Base::exitMenuRender();
-    }
-    /**
-    * @brief get the element size
-    * @return idx_t (number of collection members)
-    * @details non collection elements will return 0
-    */
-    inline idx_t size() {return data->size();}
-    inline bool selected(idx_t i) const override {return N::selected(i);}
-    inline bool enabled(idx_t i) const override {return data->enabled(i);}
-    inline void enable(idx_t n,bool o) {data->enable(n,o);}
+    // DynamicNav(Data& o):data(&o) {}
+    // inline void printMenu() {This::printMenu(*this);}
+    // template<typename Nav>
+    // inline void printMenu(Nav& nav) {
+    //   Base::out.template printMenu<Nav,Out,Data>(nav,Base::out,*Base::data);
+    // }
+    // inline bool selected(idx_t i) const override {return N::selected(i);}
+    // inline bool enabled(idx_t i) const override {return data->enabled(i);}
+    // inline void enable(idx_t n,bool o) {data->enable(n,o);}
     inline bool up() override {return N::template _up<This>(*this);}
     inline bool down() override {return N::template _down<This>(*this);}
     inline bool left() override {return N::template _left<This>(*this);}
@@ -143,11 +118,11 @@ class Nav:public NavNode,public NavBase<Out,N> {
     inline bool enter() override {return N::template _enter<This>(*this);}
     inline bool esc() override {return N::template _esc<This>(*this);}
     // inline Item& operator[](idx_t n) {return data->operator[](n);}
-    inline NavAgent activate() {return data->activate();}
-    inline NavAgent activate(idx_t n) {return data->activateItem(n);}
-    inline Modes mode() const override {return N::mode();}
-  protected:
-    Data* data;
+    // inline NavAgent activate() {return data->activate();}
+    // inline NavAgent activate(idx_t n) {return data->activateItem(n);}
+    // inline Modes mode() const override {return N::mode();}
+/*  protected:
+    Data* data;*/
 };
 
 /**
@@ -246,18 +221,22 @@ class ItemNav:public N {
     inline Modes _mode(Nav& nav) const {
       _trace(MDO<<"ItemNav::_mode"<<endl);
       return focus.result()?focus.mode():Modes::Normal;}
+    inline bool hasFocus() const {return focus;}
   protected:
     NavAgent focus;
 };
 
 //static nav tree fails here because the same navnode should be re-used for multiple target!
 template<typename N,idx_t max_depth=1>
-class NavTree:public N {
+class NavTree {
   public:
     using This=NavTree<N,max_depth>;
-
+    NavTree(typename N::DataType& data) {path[0].setTarget(data);}
     inline bool selected(idx_t n) {return path[level].selected(n);}
     inline Modes mode() {return path[level].mode();}
+    inline void enterMenuRender() {path[level].enterMenuRender();}
+    inline void exitMenuRender() {path[level].exitMenuRender();}
+    inline void newView() {path[level].newView();}
     template<typename Data>
     inline void setTarget(Data d) {path[level].setTarget(d);}
     inline idx_t size() {return path[level].size();}
@@ -265,6 +244,7 @@ class NavTree:public N {
     inline bool enabled(idx_t n) {return path[level].enabled(n);}
     inline NavAgent activate() {return path[level].activate();}
     inline NavAgent activate(idx_t n) {return path[level].activate(n);}
+    inline bool hasFocus() const {return path[level].hasFocus();}
     // inline Modes _mode() const {return path[level].mode();}
     template<typename Nav>
     inline Modes _mode(Nav& nav) {return path[level]._mode(nav);}
@@ -275,18 +255,20 @@ class NavTree:public N {
     //TODO: remove member access!
     template<typename Nav> inline bool _enter(Nav& nav) {
       bool r=path[level]._enter(nav);
-      if (r&&!N::focus) level++;
+      if (r&&!hasFocus()) level++;
       return r;
     }
-    template<typename Nav> inline bool _esc(Nav& nav) {
+    template<typename Nav>
+    inline bool _esc(Nav& nav) {
       bool r=path[level]._esc(nav);
-      if (!N::focus) level--;
+      if (!path[level].hasFocus()) level--;
       return r;
     }
     template<typename Nav> inline bool _up(Nav& nav) {return path[level]._up(nav);}
     template<typename Nav> inline bool _down(Nav& nav) {return path[level]._down(nav);}
     template<typename Nav> inline bool _left(Nav& nav) {return path[level]._left(nav);}
     template<typename Nav> inline bool _right(Nav& nav) {return path[level]._right(nav);}
+    inline idx_t pos() {return path[level].pos();}
   protected:
     idx_t level=0;
     N path[max_depth];
