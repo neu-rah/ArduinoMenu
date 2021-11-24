fea0b8ee41bbe4ded7d2313a494091158a9013d1
Rui Azevedo <ruihfazevedo@gmail.com>
2021-11-24 15:52:53 -0100

    FlashText

---
+++ src/menu/framework/Arduino/flashText.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace Menu {
+  template <const PROGMEM ConstText* text>
+  struct FlashText {
+    template <typename O = Empty<Nil>>
+    struct Part : O {
+      using Base = O;
+      using This = Part<Base>;
+      using Base::Base;
+      template <typename Nav, typename Out, bool delegate = true>
+      void printTo(Nav &nav, Out &out, int n = 0, bool sel = false) {
+        out.print(text[0]);
+        if (delegate)
+          Base::printTo(nav, out, n, sel);
+      }
+    };
+  };
+
+  template <const PROGMEM ConstText *text>
+  using FlashLabel = Chain<AsLabel::Part, FlashText<text>::template Part>;
+};
